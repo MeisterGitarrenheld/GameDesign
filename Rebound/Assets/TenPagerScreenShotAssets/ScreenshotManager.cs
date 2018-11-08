706809823f01fcda24ace1fcf4f10707d15d23b8
MeisterGitarrenheld <tom-haeschke@hotmail.de>
2018-11-08 23:58:15 +0100

    Unity Scene for Potential Screen shots
    Nur Modelle, keine Scripts,
    Kindobjekte im Scenen Objekt werden mit oben und unten hin und hergeschaltet,
    Kamerapositionen, die immer das erste Kind sind werden mit links und rechts umhergeschaltet
    Build ist dabei für Screenshots
    Neue Szenen und modelle einfach hinzufügen, sollte theoretisch einfach gehen

---
+++ Rebound/Assets/TenPagerScreenShotAssets/ScreenshotManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenshotManager : MonoBehaviour {
+
+
+    public Transform SceneContainer;
+
+    private int currentScene;
+    private int currentCameraPosition;
+
+    private void Start()
+    {
+        currentScene = 0;
+        currentCameraPosition = 0;
+        foreach (Transform child in SceneContainer)
+            child.gameObject.SetActive(false);
+
+        SceneContainer.GetChild(currentScene).gameObject.SetActive(true);
+        Camera.main.transform.position = SceneContainer.GetChild(currentScene).GetChild(0).GetChild(currentCameraPosition).position;
+        Camera.main.transform.rotation = SceneContainer.GetChild(currentScene).GetChild(0).GetChild(currentCameraPosition).rotation;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            SceneContainer.GetChild(currentScene).gameObject.SetActive(false);
+            currentScene++;
+            currentCameraPosition = 0;
+            currentScene %= SceneContainer.childCount;
+            Camera.main.transform.position = SceneContainer.GetChild(currentScene).GetChild(0).GetChild(currentCameraPosition).position;
+            Camera.main.transform.rotation = SceneContainer.GetChild(currentScene).GetChild(0).GetChild(currentCameraPosition).rotation;
+            SceneContainer.GetChild(currentScene).gameObject.SetActive(true);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            SceneContainer.GetChild(currentScene).gameObject.SetActive(false);
+            currentScene--;
+            currentCameraPosition = 0;
+            if (currentScene < 0) currentScene = SceneContainer.childCount - 1;
+            Camera.main.transform.position = SceneContainer.GetChild(currentScene).GetChild(0).GetChild(currentCameraPosition).position;
+            Camera.main.transform.rotation = SceneContainer.GetChild(currentScene).GetChild(0).GetChild(currentCameraPosition).rotation;
+            SceneContainer.GetChild(currentScene).gameObject.SetActive(true);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            currentCameraPosition--;
+            if (currentCameraPosition < 0) currentCameraPosition = SceneContainer.GetChild(currentScene).GetChild(0).childCount - 1;
+            Camera.main.transform.position = SceneContainer.GetChild(currentScene).GetChild(0).GetChild(currentCameraPosition).position;
+            Camera.main.transform.rotation = SceneContainer.GetChild(currentScene).GetChild(0).GetChild(currentCameraPosition).rotation;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            currentCameraPosition++;
+            currentCameraPosition %= SceneContainer.GetChild(currentScene).GetChild(0).childCount;
+            Camera.main.transform.position = SceneContainer.GetChild(currentScene).GetChild(0).GetChild(currentCameraPosition).position;
+            Camera.main.transform.rotation = SceneContainer.GetChild(currentScene).GetChild(0).GetChild(currentCameraPosition).rotation;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+    }
+}
