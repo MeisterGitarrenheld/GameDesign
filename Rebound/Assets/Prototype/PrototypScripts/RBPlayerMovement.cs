f51d873dc6e297fcc6761c5b6723dc9bf03196da
Julian Stockmann <julian.stockmann@googlemail.com>
2018-12-02 14:41:30 +0100

    Prototype ingame scene added

---
+++ Rebound/Assets/Prototype/PrototypScripts/RBPlayerMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RBPlayerMovement : MonoBehaviour
+{
+
+    public float MaxMoveSpeed;
+    public float RotationSpeed;
+    public float ShieldMoveSpeed;
+    public Vector3 RotationPivot;
+    public float MaxShieldHeight;
+    public float MinShieldHeight;
+    public float ShieldRotationRate;
+
+    private Rigidbody _rb;
+    private Camera _cam;
+    private Vector3 _movementVector;
+
+    //Shield Variables
+    private Transform _shieldTransform;
+    private Vector3 _shieldOffset;
+    private float _initYOffset;
+
+    void Start()
+    {
+
+        Cursor.lockState = CursorLockMode.Locked;
+
+        _rb = GetComponent<Rigidbody>();
+        _cam = Camera.main;
+        _shieldTransform = transform.Find("Shield");
+        _shieldOffset = _shieldTransform.localPosition;
+        _initYOffset = _shieldOffset.y;
+    }
+
+    void Update()
+    {
+        float xInput = Input.GetAxis("Horizontal") * MaxMoveSpeed;
+        float zInput = Input.GetAxis("Vertical") * MaxMoveSpeed;
+        float xMouseDelta = Input.GetAxis("Mouse X") * RotationSpeed;
+        float yMouseDelta = Input.GetAxis("Mouse Y") * ShieldMoveSpeed;
+
+        _movementVector = transform.right * xInput;
+        _movementVector += transform.forward * zInput;
+
+        _shieldOffset.y = Mathf.Clamp(_shieldOffset.y + yMouseDelta, MinShieldHeight, MaxShieldHeight);
+        _shieldTransform.localPosition = _shieldOffset;
+        //_shieldTransform.LookAt(_shieldTransform.position + (_shieldTransform.position - (transform.position + RotationPivot)));
+        _shieldTransform.localRotation = Quaternion.Euler((_shieldOffset.y - _initYOffset) * -ShieldRotationRate, 0, 0);
+
+        _rb.velocity = _movementVector;
+        _rb.angularVelocity = new Vector3(0, xMouseDelta, 0);
+
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = Cursor.lockState != CursorLockMode.None ? CursorLockMode.None : CursorLockMode.Locked;
+        }
+    }
+}
