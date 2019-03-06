4930e0b7c409a7aa0cccfaaaea2a739822cd82a6
Tom Haeschke <tom-niklas.haeschke@student.uni-tuebingen.de>
2019-03-06 18:35:02 +0100

    PlasmaPower

---
+++ Rebound/Assets/Scripts/IngameGamePlayTest/RBIGPowers.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RBIGPowers : MonoBehaviour {
+
+
+    public GameObject Plasma;
+    public float PlasmaSpeed;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+            ShootPlasma();    
+    }
+
+    void ShootPlasma()
+    {
+        var plasma = Instantiate(Plasma, transform.position + transform.forward * 10, transform.rotation).GetComponent<Rigidbody>();
+        plasma.velocity = transform.GetChild(1).forward * PlasmaSpeed;
+        plasma.transform.rotation = Quaternion.LookRotation(transform.GetChild(1).forward, transform.GetChild(1).up);
+        Destroy(plasma, 20);
+    }
+
+}
