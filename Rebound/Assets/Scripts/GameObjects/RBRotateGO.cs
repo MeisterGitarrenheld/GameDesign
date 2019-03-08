ec13e4475c5ec3ed306b4dab49dcc56803a97bc4
Michael H <MichaelH45@web.de>
2019-03-08 14:52:48 +0100

    Added particle effects to powerups

---
+++ Rebound/Assets/Scripts/GameObjects/RBRotateGO.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RBRotateGO : MonoBehaviour
+{
+
+    public float RotateionSpeed = 50.0f;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(Vector3.right * RotateionSpeed * Time.deltaTime);
+	}
+}
