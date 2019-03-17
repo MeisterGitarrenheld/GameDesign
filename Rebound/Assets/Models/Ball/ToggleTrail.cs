837eaacb38bb55d9fc19e86e913e6c8b784bdc45
Wundersam <wunderlich.samuel2@gmail.com>
2019-03-17 16:55:39 +0100

    modify: Playermodel4, ball, add new prefab for ball

---
+++ Rebound/Assets/Models/Ball/ToggleTrail.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(TrailRenderer))]
+
+public class ToggleTrail : MonoBehaviour {
+    public float minimumVelocity;
+
+    private Rigidbody BallRigidBody;
+    private TrailRenderer renderer;
+    // Use this for initialization
+    void Start () {
+        BallRigidBody = this.GetComponentInParent<Rigidbody>();
+        renderer = this.GetComponent<TrailRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(BallRigidBody.velocity.magnitude > minimumVelocity && !renderer.enabled )
+        {
+            renderer.enabled = true;
+        }else if(BallRigidBody.velocity.magnitude < minimumVelocity && renderer.enabled)
+        {
+            renderer.enabled = false;
+        }
+	}
+}
