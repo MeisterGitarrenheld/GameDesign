7cf3b68c598884b59594c1106e5a7c123039eee2
Julian Stockmann <julian.stockmann@googlemail.com>
2019-03-24 00:51:45 +0100

    Ragdoll spawn, ball + mine movement sync

---
+++ Rebound/Assets/Scripts/Abilities/Wall/RBAbilityWallRagdollActivator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RBAbilityWallRagdollActivator : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _ragdollPrefab;
+
+    [SerializeField]
+    private float _ragdollLifeTime = 3.0f;
+
+    void OnDisable()
+    {
+        var ragdoll = Instantiate(_ragdollPrefab, transform.position, transform.rotation);
+        Destroy(ragdoll, _ragdollLifeTime);
+    }
+}
---
+++ Rebound/Assets/Scripts/Network/RBMotionSync.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class RBMotionSync : NetworkBehaviour
+{
+    [SyncVar]
+    private Vector3 _syncPos;
+
+    [SyncVar]
+    private Vector3 _syncRot;
+
+    private Vector3 _lastPos;
+    private Quaternion _lastRot;
+    private Transform _myTransform;
+
+    [SerializeField]
+    private float _lerpRate = 10;
+
+    [SerializeField]
+    private float _posThreshold = 0.5f;
+
+    [SerializeField]
+    private float _rotThreshold = 0.0f;
+
+    [SerializeField]
+    private float _snapThreshold = 1.0f;
+
+    void Start()
+    {
+        _myTransform = transform;
+    }
+
+    void Update()
+    {
+        TransmitMotion();
+        LerpMotion();
+    }
+
+    void TransmitMotion()
+    {
+        if (!isServer)
+        {
+            return;
+        }
+
+        if (Vector3.Distance(_myTransform.position, _lastPos) > _posThreshold || Quaternion.Angle(_myTransform.rotation, _lastRot) > _rotThreshold)
+        {
+            _lastPos = _myTransform.position;
+            _lastRot = _myTransform.rotation;
+
+            _syncPos = _myTransform.position;
+            _syncRot = _myTransform.localEulerAngles;
+        }
+    }
+
+    void LerpMotion()
+    {
+        if (isServer)
+        {
+            return;
+        }
+
+        if (Vector3.Distance(_myTransform.position, _syncPos) > _snapThreshold)
+        {
+            _myTransform.position = _syncPos;
+            _myTransform.rotation = Quaternion.Euler(_syncRot);
+        }
+        else
+        {
+            _myTransform.position = Vector3.Lerp(_myTransform.position, _syncPos, Time.deltaTime * _lerpRate);
+            _myTransform.rotation = Quaternion.Lerp(_myTransform.rotation, Quaternion.Euler(_syncRot), Time.deltaTime * _lerpRate);
+        }
+    }
+
+
+}
